2c88c66b79f3abcb7a966433525b96f4079170bd
Mai Kue Lor and Paula Vun <paula@>
2019-02-20 18:52:47 -0800

    calculator update exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,33 @@
-var add = function(number1, number2){
-return number1 + number2;
-};
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+    var add = function(number1, number2){
+    return number1 + number2;
+    };
+    var subtract = function(number1, number2){
+      return number1 - number2;
+    };
+    var multiply = function(number1, number2){
+    return number1 * number2;
+    };
+    var divide = function(number1, number2){
+      return number1 / number2;
+    };
 
-var subtract = function(number1, number2){
-return number1 - number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert(result);
-
-var multiply = function(number1, number2){
-return number1 * number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
-
-var divide = function(number1, number2){
-return number1 / number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
-
-var bodyMass= function(number1, number2){
-return (number1 * 703) / (number2 * number2);
-};
-
-var number1 = parseInt(prompt("Enter your weight in pounds:"));
-var number2 = parseInt(prompt("Enter your height in inches:"));
-var result = bodyMass(number1, number2);
-alert(result);
-
-var temp= function(number1){
-return (number1 * 9/5 + 32)
-};
-
-var number1 = parseInt(prompt("Enter degrees in Celsius:"));
-var result = temp(number1);
-alert(result);
-
-var gL= function(number1){
-return (number1 * 3.78541)
-};
-
-var number1 = parseInt(prompt("Enter measurement in gallons"));
-var result = gL(number1);
-alert(result);
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
